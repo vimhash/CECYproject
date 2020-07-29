d55beea0e2ece458f6bb9e1abd89a8eeee38cd90
Omar Cedeño <grarogccee@gmail.com>
2020-07-28 22:15:36 -0500

    docentes-cursos

--- Angular/src/app/pages/cecy/matriculacion/dashboards/docentes/docentes.component.ts
+++ Angular/src/app/pages/cecy/matriculacion/dashboards/docentes/docentes.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from "@angular/core";
-import { SelectItem, MenuItem } from "primeng/api";
+import { SelectItem } from "primeng/api";
 import { BreadcrumbService } from "../../../../../shared/breadcrumb/breadcrumb.service";
-import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: "app-docentes",
   templateUrl: "./docentes.component.html",
 })
 export class DocentesComponent implements OnInit {
+      cities: SelectItem[];
   constructor(
-    private breadcrumbService: BreadcrumbService,
-    private spinner: NgxSpinnerService
-  ) {
+    private breadcrumbService: BreadcrumbService  ) {
     this.breadcrumbService.setItems([
       { label: "CEC-Y", routerLink: ["/cecy/dashboard/docentes"] },
     ]);
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.cities = [];
+    this.cities.push({ label: 'JUNIO 2020 - OCTUBRE 2020', value: { id: 1, name: 'JUNIO 2020 - OCTUBRE 2020', code: 'JO' } });
+    this.cities.push({ label: 'SEPTIEMBRE 2020 - ABRIL 2020', value: { id: 2, name: 'SEPTIEMBRE 2020 - ABRIL 2020', code: 'SA' } });
+  }
 }
