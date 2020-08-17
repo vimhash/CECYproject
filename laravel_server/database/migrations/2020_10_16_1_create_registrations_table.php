1870eefc71b08e49f673a33915ec9c12e7766bfd
Omar Cedeño <grarogccee@gmail.com>
2020-08-17 11:19:23 -0500

    Periodoacademico&matricula

---
+++ laravel_server/database/migrations/2020_10_16_1_create_registrations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('pgsql-cecy')->create('registrations', function (Blueprint $table) {
+            $table->id();
+            $table->string('date_registration', 50);
+            $table->boolean('approved');
+            $table->integer('number_registration');
+            $table->foreignId('state_id')->constrained();
+            $table->foreignId('type_id')->constrained('catalogues');
+            $table->foreignId('period_school_id')->constrained('periodschools');
+              });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('pgsql-cecy')->dropIfExists('registrations');
+    }
+}
